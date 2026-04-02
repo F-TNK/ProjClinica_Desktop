fa8092c535e5a38bbafbd9105b1e2d3f1de5d8dc
F-TNK <fabiotanakasantos@gmail.com>
2026-04-02 16:17:21 -0300

    box_disable

--- src/main/java/view/Home.java
+++ src/main/java/view/Home.java
@@ -154,7 +154,6 @@ public class Home extends javax.swing.JFrame {
             }
         });
 
-        mesBox.setEnabled(false);
         mesBox.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 mesBoxActionPerformed(evt);
@@ -173,7 +172,7 @@ public class Home extends javax.swing.JFrame {
         jLabel3.setForeground(new java.awt.Color(200, 246, 246));
         jLabel3.setText("3. Uma mês");
 
-        jComboBox4.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox4.setEnabled(false);
         jComboBox4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jComboBox4ActionPerformed(evt);
